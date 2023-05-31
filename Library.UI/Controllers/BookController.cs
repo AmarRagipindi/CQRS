e48a776a6cfcdea9cebf5194441c46acbff2bc42
Amar Ragipindi <amaranatha.ragipindi@gmail.com>
2023-05-31 21:59:32 +0530

    Initial code with fluent validations

---
+++ Library.UI/Controllers/BookController.cs
@@ -0,0 +1,90 @@
+﻿using BooksPortal.UI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksPortal.UI.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: BookController
+        public ActionResult Index()
+        {
+            BookViewModel bv = new BookViewModel();
+            bv.Name = "This is";
+            bv.Rating = 1m;
+            BookViewModelValidator validationRules = new BookViewModelValidator();
+            var result = validationRules.Validate(bv);
+
+            return View(bv);
+        }
+
+        // GET: BookController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BookController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Library.UI/Models/BookViewModel.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+namespace BooksPortal.UI.Models
+{
+    public class BookViewModel
+    {
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+        public string? Code { get; set; }
+        public decimal Price { get; set; }
+        public decimal Rating { get; set; }
+
+    }
+    public class BookViewModelValidator : AbstractValidator<BookViewModel>
+    {
+        public BookViewModelValidator() {
+            RuleFor(book => book).NotNull();
+            RuleFor(book=> book.Name).Length(1,20).WithMessage("Book name is too long");
+            RuleFor(book => book.Rating).LessThanOrEqualTo(5).WithMessage("Raing should be between 1 to 5");
+
+        }
+    }
+}
